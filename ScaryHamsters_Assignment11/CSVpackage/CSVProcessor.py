55fa1256b74d7bfcf0ee4999041b1de801e8bd04
Abdoulch197 <165812403+Abdoulch197@users.noreply.github.com>
2024-11-21 00:46:42 -0500

    updated docummentation on CSVProcessor.py and main.py

--- ScaryHamsters_Assignment11/CSVpackage/CSVProcessor.py
+++ ScaryHamsters_Assignment11/CSVpackage/CSVProcessor.py
@@ -1,7 +1,13 @@
-
-# Brief Description of what this module does.   
+# Name: Sidney Huschart, Shelby Sash, Roman Stryjewski, Cheikh Abdoul
+# email:  huschash@mail.uc.edu, sashsk@mail.uc.edu, sashsk@mail.uc.edu, abdoulch@mail.uc.edu
+# Assignment Number: Assignment 11
+# Due Date:   November 21 2024
+# Course #/Section:   IS4010-001
+# Semester/Year:   Fall 2024
+# Brief Description of the assignment: Collaborate using github to clean data in a csv file
+# Brief Description of what this module does: Wrote the code to clean up the  data in FuelPurchaseData csv file. Crested two new files with new sets of data.
 # Citations:
-# Anything else that's relevant: 
+# Anything else that's relevant:
 
 
 # CSVProcessor.py
